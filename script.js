56d7f740abe5e4fd5e4ae1658cd5d93c2208c69e
Kavindu Ranawaka <knranawaka@std.appsc.sab.ac.lk>
2024-09-08 14:58:02 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const hero = document.querySelector("hero"),
+      heroBoy=document.querySelector(".heroBoy"),
+      vilan=document.querySelector(".vilan");
+
+function jump(){
+    if (hero.classList != "animate"){
+       hero.classList.add("animate");
+    }
+}
+
+document.addEventListener("keydown", function(){
+    jump();
+})
